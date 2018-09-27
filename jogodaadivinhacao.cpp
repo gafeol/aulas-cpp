87f436bcd2a48078540f4e32d800ec7f83e88a5b
gafeol <gabfeol@gmail.com>
2018-09-27 08:51:18 -0300

    Aula 2.1.4

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -14,4 +14,15 @@ int main()
     cin >> chute;
 
     cout << "O valor do seu chute é: " << chute << endl;
+
+    if(chute == numero_secreto){
+        cout << "Parabéns! Você acertou o número secreto!" << endl;
+    }
+    else if(chute > numero_secreto){
+        cout << "Seu chute foi maior que o número secreto!" << endl;
+    }
+    else {
+        cout << "Seu chute foi menor que o número secreto!" << endl;
+    }
+
 }
